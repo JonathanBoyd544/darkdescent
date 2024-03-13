e8783c4c9ea86a9d4515264612aa955d333f54b3
drroid <121253138+drroid@users.noreply.github.com>
2024-03-12 17:24:33 -0700

    Initial upload

---
+++ progresstest.py
@@ -0,0 +1,15 @@
+from progress.bar import Bar
+import time
+from random import randint
+import os
+
+playerLvl = 0
+xpForLevel = 50
+
+print("These are your statistics:")
+with Bar(" |=========== Level {} ============|\n".format(playerLvl), fill='=', max=xpForLevel) as bar:
+    for i in range(xpForLevel):
+        bar.next(randint(1, xpForLevel))
+        time.sleep(1)
+        os.system("cls")
+    print("Success!")
